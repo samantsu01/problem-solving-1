3a46ce5f5227a8d2b372791b99f92267ec112309
samantsu01 <63949428+samantsu01@users.noreply.github.com>
2022-12-15 12:55:26 +0530

    Running Sum of 1d Array

---
+++ RunningSum.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class RunningSum {
+    public static int[] runningSum(int[] nums) {
+
+        for(int i = 0 ; i <= nums.length-1 ; i++){
+            if(i == 0){
+                nums[i] = nums[i];
+            }else{
+                nums[i] = nums[i - 1] + nums[i];
+            }
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(runningSum(nums)));
+    }
+}
